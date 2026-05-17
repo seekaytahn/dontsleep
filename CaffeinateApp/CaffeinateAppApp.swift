8aa9982c9593e3812611fca10f544ab3ab29f64c
seekay <seekay@xxx.com>
2026-05-17 20:41:56 +0800

    Initial Commit

---
+++ CaffeinateApp/CaffeinateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinateAppApp.swift
+//  CaffeinateApp
+//
+//  Created by Chor Kiong Tan on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaffeinateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaffeinateApp
+//
+//  Created by Chor Kiong Tan on 17/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
